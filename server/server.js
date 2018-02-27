49a2d9eef4f93bb5d48876c38ee8ccd18e131067
wangzhonghuan <wangzhonghuan@58ganji-corp.com>
2018-02-27 18:28:14 +0800

    test server

--- server/server.js
+++ server/server.js
@@ -5,19 +5,23 @@ const config = require('./config')
 
 const app = express()
 
-mongoose.connect(`mongodb://localhost/{config.database}`);
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('MongoDB connected...')
-});
+// mongoose.connect(`mongodb://localhost/{config.database}`);
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log('MongoDB connected...')
+// });
 
 const port = process.env.PORT || 3001
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// route
 
+app.get('/', (req, res) => {
+	res.send('<h1>This is the test<h1>')
+})
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`)
