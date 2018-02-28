7bdec77e03bde7a09b1259d790640bd1f976d980
wangzhonghuan <wangzhonghuan@58ganji-corp.com>
2018-02-28 17:40:54 +0800

    add map component

--- client/src/components/map.vue
+++ client/src/components/map.vue
@@ -1,32 +1,27 @@
 <template>
     <div class="map-container">
-        <h1>This is the map container</h1>
+        <vl-map :load-tiles-while-animating="true" :load-tiles-while-interacting="true" style="height: 400px">
+            <vl-view :center.sync="center" :zoom.sync="zoom" :rotation.sync="rotation"></vl-view>
+
+            <vl-layer-tile>
+                <vl-source-osm></vl-source-osm>
+            </vl-layer-tile>
+        </vl-map>
     </div>
 </template>
 <script>
 export default {
-  data() {
-      return {
-          noData: false,
-          data: null
-      }
-  },
-  methods: {
-      getData() {
-          this.$http.get('')
-            .then(response => {
-                this.data = response.data
-            })
-            .catch(error => {
-                this.noData = false
-            })
-      },
-    created() {
-        alert('Test')
+    data() {
+        return {
+            center: [0, 0],
+            zoom: 3,
+            rotation: 0
+        }
     }
   }
-}
 </script>
 <style scoped>
-
+    .map-container {
+        border: 1px solid rgba(0, 0, 0, 0.1);
+    }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 // import router from './router'
-
+import { Map, TileLayer, OsmSource } from 'vuelayers'
+import 'vuelayers/lib/style.css'
 Vue.config.productionTip = false
+
+Vue.use(Map)
+Vue.use(TileLayer)
+Vue.use(OsmSource)
 
 /* eslint-disable no-new */
 new Vue({
